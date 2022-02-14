5121a0aa604decb79e5707b787f94019c9e4661c
R-Rijnbeek <robert270384@gmail.com>
2022-02-14 15:19:54 +0100

    put transformation matrix test

--- TEST/test.py
+++ TEST/test.py
@@ -7,3 +7,6 @@ if __name__ == '__main__':
     pr = GeometricManipulation()
     result = pr.Absolute3DDistance([1,4,6.7],[2,2,2])
     print(result)
+
+    a=pr.CombinationOfTransformationMatrix3x3([[1,2,3],[5,6,7],[8,9,10]],[[1,0,0],[1,1,0],[0,0,1]])
+    print(a)
